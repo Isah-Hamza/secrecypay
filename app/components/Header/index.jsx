ed093b28b88b8368b9e9f475bfd6958077f7b79c
Isah-Hamza <hamzolium@gmail.com>
2023-12-09 17:52:13 +0100

    minor adjustments

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -7,8 +7,8 @@ const index = ({ scrollToForm }) => {
   return (
     <header className="max-w-[1300px] m-auto px-5 relative z-20 flex justify-between items-center py-10">
       <div className="flex items-center gap-1 ">
-        <Image src={logo} alt="logo" width={20} height={30} />
-        <span>SECRECY PAY</span>
+        <Image src={logo} alt="logo" width={25} height={35} />
+        <span className="font-extrabold text-xl">SECRECY PAY</span>
       </div>
       <button
         onClick={scrollToForm}
